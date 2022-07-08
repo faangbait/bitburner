626494a73c58245c7dacf6d70613aa778cfe4111
S Smith <root@madeof.glass>
2022-07-08 09:04:32 -0500

    Initial Commit. Removed deprecated functionality.
    .ns is deprecated as a file extension. modified tsconfig to use .js.

---
+++ src/bin/runPhoenix.ts
@@ -0,0 +1,11 @@
+import {NS} from "Bitburner";
+import {TermLogger} from "/lib/Helpers";
+
+/** @param {NS} ns **/
+export async function main(ns: NS) {
+    const logger = new TermLogger(ns);
+
+    logger.log("\tPhoenix rises from the ashes...");
+    
+
+}
--- src/bin/sayHelloTs.ts
+++ src/bin/sayHelloTs.ts
@@ -8,5 +8,5 @@ export async function main(ns: NS) {
     logger.log("Hello from TypeScript o/");
     logger.info("\tEverything seems to be in order :D");
     logger.warn("\tJust showing some colors");
-    logger.err("Fake error, no panik");
+    logger.err("\tFake error, no panik");
 }
